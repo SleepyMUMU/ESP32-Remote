39ada92c7b9b217c77240449479ac3c22ae9a906
5gugu <107771684+SleepyMUMU@users.noreply.github.com>
2024-04-14 04:33:08 +0000

    FreeRTOS Enabled.

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,27 @@
 #include <Arduino.h>
 
+void task1(void *pvParameters)
+{
+  for (;;)
+  {
+    Serial.println("Task 1");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
 // put function declarations here:
 int myFunction(int, int);
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  xTaskCreate(task1, "Task 1", 1000, NULL, 1, NULL);
+  vTaskStartScheduler();
+  Serial.println("Setup");
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
