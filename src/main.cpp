41200eaf0be9c044864966abf2b42b2cbcb06162
5gugu <107771684+SleepyMUMU@users.noreply.github.com>
2024-04-14 05:11:14 +0000

    Add FashionStar_UartServo library and initialize servo in setup()

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,54 @@
 #include <Arduino.h>
+#include <FashionStar_UartServo.h>
+#include <FashionStar_UartServoProtocol.h>
+
+#define SERVO_ID 0x01
+
+//初始化舵机通讯协议
+FSUS_Protocol protocol;
+
+//绑定舵机到协议
+FSUS_Servo servo(SERVO_ID, &protocol);
 
 void task1(void *pvParameters)
 {
   for (;;)
   {
-    Serial.println("Task 1");
+    Serial.println("Task 1 is running");
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
+String inputString = "";         // a string to hold incoming data
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup()
+void SerialCom(void *pvParameters)
 {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  xTaskCreate(task1, "Task 1", 1000, NULL, 1, NULL);
-  vTaskStartScheduler();
-  Serial.println("Setup");
+  for (;;)
+  {
+    if (Serial.available() > 0)
+    {
+      while (Serial.available() > 0)
+      {
+        inputString += (char)Serial.read();
+      }
+    }
+  }
 }
 
-void loop()
-{
-  // put your main code here, to run repeatedly:
-}
+  void setup()
+  {
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+
+    servo.init();
+    servo.setSpeed(100.0);
+    servo.setAngle(0);
+    
+    xTaskCreate(task1, "Task 1", 1000, NULL, 1, NULL);
+    xTaskCreate(SerialCom, "SerialCom", 1000, NULL, 1, NULL);
+    vTaskStartScheduler();
+  }
+
+  void loop()
+  {
+    // put your main code here, to run repeatedly:
+  }
